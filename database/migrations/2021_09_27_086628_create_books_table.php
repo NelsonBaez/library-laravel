fb1bc703b47e9e65a4fb56278e72ddf42f918961
Nelson Baez <bluexbaez@gmail.com>
2021-09-27 13:25:35 -0300

    feito migrations

---
+++ database/migrations/2021_09_27_086628_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 500);
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('writer_id')->constrained();
+            $table->integer('pages');
+            $table->timestamps();
+            $table->index('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
